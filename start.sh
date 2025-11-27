3f68c623aa524b608f806d3d5c9cd548b4ffc230
onlyformynew-cpu <only.for.my.new.game@gmail.com>
2025-11-27 23:32:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-set -e
-# Перехід у каталог, де лежить скрипт
-cd "$(dirname "$0")"
-# Встановлення залежностей
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-# Інсталяція браузерів для playwright
-python3 -m playwright install
-# Запуск основного скрипта
-python3 dtek_checker.py
+playwright install chromium
+python dtek_checker.py
